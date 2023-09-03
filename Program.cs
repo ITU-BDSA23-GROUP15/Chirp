dcdcb99bdcd013d0e32b2f2dc06bd9463a267006
Rasmus Lundahl Nielsen <rasmun@MacBook-Pro-tilhrende-Rasmus.local>
2023-09-03 21:53:16 +0200

    fix formatting/join method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     // The following block of code is inspired and adapted from: https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp/34265869#34265869
     public static void Main ()
     {
-    string fileName = "/mnt/c/Users/mchrn/ITU/3_Semester/Chirp/chirp_cli_db.csv";
+    string fileName = Environment.CurrentDirectory + @"/chirp_cli_db.csv";
     string[] allCheeps;
         using (StreamReader reader = new StreamReader(fileName))
     {
@@ -21,7 +21,7 @@ class Program
             allCheeps = CSVParser.Split(line);
 
             /* Do something with X */
-            Console.WriteLine(string.Join("\n", allCheeps));
+            Console.WriteLine(string.Join(", ", allCheeps));
             // Array.ForEach(allCheeps, Console.WriteLine);
         }
         
