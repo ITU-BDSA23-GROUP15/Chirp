a48c9910eac8e41eedddfae69397a2085ca876f2
Rasmus <raln@itu.dk>
2023-09-19 21:58:41 +0200

    Add database tests
    Added tests for the CSVDB and added
    arrange, act, assert keywords.
    
    Co-authored-by: Mads <macj@itu.dk>
    Co-authored-by: Frederik <frlr@itu.dk>

--- test/Chirp.SimpleDB.Tests/CSVDatabaseTests.cs
+++ test/Chirp.SimpleDB.Tests/CSVDatabaseTests.cs
@@ -4,17 +4,18 @@ public class CSVDatabaseTests
 {
     [Fact]
     public void StoreAndReadCheepsTest() {
+        // Arrange
         var cheeps = new List<Cheep>
         {
             new Cheep("testUser", "testCheep", 1690981487),
             new Cheep("testUser2", "testCheep2", 1690981487)
         };
-
+        // Act
         var database = CSVDatabase<Cheep>.Instance;
         database.Store(cheeps);
 
         var readCheeps = database.Read(10);
-
+        // Assert
         Assert.Equal(cheeps, readCheeps);
     }
 }
