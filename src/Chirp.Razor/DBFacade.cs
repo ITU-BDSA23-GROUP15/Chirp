b2ab5f14982fc4dae94cf826a1739b0add113dce
Jacob Pærregaard <jacp@itu.dk>
2023-10-04 12:00:30 +0200

    removes database from github and redesigns dbfacade to make database itself

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,7 +1,50 @@
 ﻿using Microsoft.Data.Sqlite;
 using Chirp.CheepService;
+using System.Reflection;
 public class DBFacade {
-    private string sqlDBFilePath = Environment.CurrentDirectory + @"/data/data.db";
+
+    public DBFacade()
+    {
+        CreateDatabase();
+        Console.WriteLine(connString);
+    }
+
+    private string connString;
+    private void CreateDatabase()
+    {
+        var builder = new SqliteConnectionStringBuilder
+        {
+            DataSource = Environment.CurrentDirectory + @"/data/data.db",
+        };
+        connString = builder.ToString();
+
+        // create schema
+        ExecuteSQLQuery("Chirp.Razor.data.schema.sql");
+
+        // insert content
+        ExecuteSQLQuery("Chirp.Razor.data.dump.sql");
+    }
+
+    private void ExecuteSQLQuery(string filename) 
+    {
+        using(var connection = new SqliteConnection(connString))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            
+            command.CommandText = ReadResourceFile(filename);
+
+            command.ExecuteNonQuery();
+        }
+    }
+    private string ReadResourceFile(string filename) 
+    {
+        //return Assembly.GetExecutingAssembly().GetManifestResourceNames(); // testing purpose
+        var thisAssembly = Assembly.GetExecutingAssembly();
+        using var stream = thisAssembly.GetManifestResourceStream(filename);
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
     public List<CheepViewModel> GetCheeps(int offset = 1) 
     {
         List<CheepViewModel> cheeps = new List<CheepViewModel>();
@@ -11,7 +54,7 @@ public class DBFacade {
                            ORDER by m.pub_date desc
                            LIMIT 32 OFFSET {offset}";
 
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        using (var connection = new SqliteConnection(connString))
         {
             connection.Open();
 
