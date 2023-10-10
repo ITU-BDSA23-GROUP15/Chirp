a83dd98546210bc3a002f45583fc7ffd4094692c
Frederik Lund Rosenlund <118807428+frederiklr99@users.noreply.github.com>
2023-10-10 10:39:26 +0200

    list was with small 'l' fixed to capital 'L' Co-authored-by: Daniel <dmil@itu.dk>

--- Chirp.Razor/Models/Author.cs
+++ Chirp.Razor/Models/Author.cs
@@ -2,5 +2,5 @@ public class Author
 {
     public required string Name { get; set; }
     public string Email { get; set; }
-    public list<Cheep> Cheeps { get; set; }
+    public List<Cheep> Cheeps { get; set; }
 }
