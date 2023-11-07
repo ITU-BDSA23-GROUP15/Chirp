5c9ffc6db6440fe15eab1f18177f09c8d677c0e7
Daniel Millard <dmil@itu.dk>
2023-11-07 12:13:00 +0100

    Cleanup old dependencies for real this time
    Co-authored-by: Frederik <frlr@itu.dk>
    Co-authored-by: Jacob <jacp@itu.dk>

--- src/Chirp.Infrastructure/ChirpContext.cs
+++ src/Chirp.Infrastructure/ChirpContext.cs
@@ -12,9 +12,6 @@ public class ChirpContext : DbContext
         
     }
 
-    // protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //     => options.UseSqlite($"Data source={GetDbPath()}");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Author>().Property(a => a.AuthorId).IsRequired();
@@ -24,11 +21,4 @@ public class ChirpContext : DbContext
         modelBuilder.Entity<Author>().HasIndex(a => a.Name).IsUnique();
         modelBuilder.Entity<Author>().HasIndex(a => a.Email).IsUnique();
     }
-
-    // private static string GetDbPath()
-    // {
-    //     var folder = Environment.SpecialFolder.LocalApplicationData;
-    //     var path = Environment.GetFolderPath(folder);
-    //     return Path.Join(path, "chirp.db");
-    // }
 }
