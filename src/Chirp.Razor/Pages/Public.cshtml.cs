7db68e3337dc27fe101502b18c4980cfc4f6995e
Frederik Lund Rosenlund <118807428+frederiklr99@users.noreply.github.com>
2023-11-08 15:55:49 +0100

    Implement post functionality
    Get email and username
    Create Author if it does not exist
    Create Cheep
    
    Co-authored-by: Daniel <dmil@itu.dk>

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -6,11 +6,13 @@ namespace Chirp.Razor.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepRepository _cheepRepository;
+    private readonly IAuthorRepository _authorRepository;
     public List<CheepDto> Cheeps { get; set; }
 
-    public PublicModel(ICheepRepository cheepRepository)
+    public PublicModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
     {
         _cheepRepository = cheepRepository;
+        _authorRepository = authorRepository;
         Cheeps = new List<CheepDto>();
     }
     public async Task<IActionResult> OnGet([FromQuery(Name = "page")] int pageIndex = 1)
@@ -27,8 +29,17 @@ public class PublicModel : PageModel
         {
             return RedirectToPage();
         }
+
+        var email_claim = User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Email);
+        string email = email_claim!.Value;
+        string userName = User.Identity!.Name!;
+
+        if (!await _authorRepository.AuthorExists(userName)){
+            _authorRepository.CreateAuthor(new CreateAuthorDto(userName, email));
+        }
         
-        
+        _cheepRepository.CreateCheep(new CreateCheepDto(message, userName));
+
         return RedirectToPage();
     }
 }
