f55875031e58b1ea4757b07d86c2b044633dbd33
Jacob Pærregaard <jacp@itu.dk>
2023-11-08 15:12:43 +0100

    integration test on webpage using docker
    removed other tests
    
    Co-authored-by: Rasmus <raln@itu.dk>

--- test/Chirp.IntegrationTests/GlobalUsings.cs
+++ test/Chirp.IntegrationTests/GlobalUsings.cs
@@ -2,4 +2,3 @@ global using Xunit;
 global using AutoFixture;
 global using Chirp.Infrastructure;
 global using Testcontainers.MsSql;
-global using MediatR;
---
+++ test/Chirp.IntegrationTests/RazorTest.cs
@@ -0,0 +1,46 @@
+namespace Application.IntegrationTests;
+
+public class RazorTest : BaseIntegrationTest
+{
+	private readonly HttpClient _client;
+	public RazorTest(IntegrationTestWebAppFactory factory) : base(factory)
+	{
+		_client = factory.CreateClient();
+	}
+
+    [Fact]
+    public async void CanSeePublicTimeline()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("Helge")]
+    [InlineData("Rasmus")]
+    public async void CanSeePrivateTimeline(string author)
+    {
+        var response = await _client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
+    }
+
+    [Fact]
+    public async void GetAuthorByName()
+    {
+		DbContext.Database.EnsureCreated();
+		DbContext.Authors.Add(new Author { Name = "testuser", Email = "testuser@gmail.com", Cheeps = new List<Cheep>() });
+		await DbContext.SaveChangesAsync();
+
+		var repository = new AuthorRepository(DbContext);
+		var author = repository.GetAuthorByName("testuser");
+		Assert.Equal("testuser", author.Result.Name);
+    }
+}
