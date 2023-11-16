f7c64fcc65d906da9f7d0454e758b1ca2f4456e8
Jacob Pærregaard <jacp@itu.dk>
2023-11-16 23:16:08 +0100

    remove cheep-injection and change to dto

--- test/Chirp.Testing/RepositoryTest/AuthorUnitTests.cs
+++ test/Chirp.Testing/RepositoryTest/AuthorUnitTests.cs
@@ -6,26 +6,16 @@ namespace Application.Testing;
 
 public class AuthorUnitTests : BaseIntegrationTest
 {
-	private readonly CheepRepository cheepRepository;
 	private readonly AuthorRepository authorRepository;
-	private readonly Faker<Author> authorGenerator;
-	private readonly Faker<Cheep> cheepGenerator;
+	private readonly Faker<CreateAuthorDto> authorGenerator;
+
 	public AuthorUnitTests(IntegrationTestWebAppFactory factory) : base(factory)
 	{
-		cheepRepository = new CheepRepository(DbContext);
 		authorRepository = new AuthorRepository(DbContext);
 
-		authorGenerator = new Faker<Author>()
-			.RuleFor(u => u.AuthorId, (f, u) => f.Random.Guid())
+		authorGenerator = new Faker<CreateAuthorDto>()
 			.RuleFor(u => u.Name, (f, u) => f.Name.LastName())
 			.RuleFor(u => u.Email, (f, u) => f.Internet.Email());
-
-		cheepGenerator = new Faker<Cheep>()
-			.RuleFor(u => u.CheepId, (f, u) => f.Random.Guid())
-			.RuleFor(u => u.Author, (f, u) => authorGenerator.Generate()) // generate new author
-			.RuleFor(u => u.AuthorId, (f, u) => u.Author.AuthorId) // take fake author's id
-			.RuleFor(u => u.Text, (f, u) => f.Lorem.Sentence()) // generate random sentence
-			.RuleFor(u => u.TimeStamp, (f, u) => f.Date.Past()); // generate random date in the past
 	}
 
 	[Fact]
@@ -49,7 +39,7 @@ public class AuthorUnitTests : BaseIntegrationTest
 	{
 		// Arrange
 		var author = authorGenerator.Generate();
-		await authorRepository.CreateAuthor(new CreateAuthorDto(author.Name, author.Email));
+		await authorRepository.CreateAuthor(author);
 
 		// Act
 		// Assert
@@ -60,7 +50,7 @@ public class AuthorUnitTests : BaseIntegrationTest
 	public async Task GetAuthorByName() {
 		// Arrange
 		var author = authorGenerator.Generate();
-		await authorRepository.CreateAuthor(new CreateAuthorDto(author.Name, author.Email));
+		await authorRepository.CreateAuthor(author);
 
 		// Act
 		var authorDto = await authorRepository.GetAuthorByName(author.Name);
@@ -84,7 +74,7 @@ public class AuthorUnitTests : BaseIntegrationTest
 	public async Task GetAuthorByEmail() {
 		// Arrange
 		var author = authorGenerator.Generate();
-		await authorRepository.CreateAuthor(new CreateAuthorDto(author.Name, author.Email));
+		await authorRepository.CreateAuthor(author);
 
 		// Act
 		var authorDto = await authorRepository.GetAuthorByEmail(author.Email);
