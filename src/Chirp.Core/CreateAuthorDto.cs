38b7d63d86900c7a1f7a9e2d49a4bde3890ca81d
Jacob Pærregaard <jacp@itu.dk>
2023-11-16 23:25:56 +0100

    add parameterless constructor to satisfy test

--- src/Chirp.Core/CreateAuthorDto.cs
+++ src/Chirp.Core/CreateAuthorDto.cs
@@ -1,2 +1,18 @@
 namespace Chirp.Core;
-public record CreateAuthorDto(string Name, string Email);
+
+public record CreateAuthorDto
+{
+	public string? Name { get; init; }
+	public string? Email { get; init; }
+
+	public CreateAuthorDto(string Name, string Email)
+	{
+		this.Name = Name;
+		this.Email = Email;
+	}
+
+	public CreateAuthorDto() // parameterless constructor is created for the purpose of the unit tests
+	{
+	}
+
+}
--- src/Chirp.Core/CreateCheepDto.cs
+++ src/Chirp.Core/CreateCheepDto.cs
@@ -1,2 +1,18 @@
 namespace Chirp.Core;
-public record CreateCheepDto(string Text, string Author);
+
+public record CreateCheepDto
+{
+	public string? Text { get; init; }
+	public string? Author { get; init; }
+
+	public CreateCheepDto(string Text, string Author)
+	{
+		this.Text = Text;
+		this.Author = Author;
+	}
+
+	public CreateCheepDto() // parameterless constructor is created for the purpose of the unit tests
+	{
+	}
+
+}
