152563ec205d5f93990adcf4c6897792bf2a1a73
Rasmus <raln@itu.dk>
2023-11-29 14:56:24 +0100

    add followers/following to about me page

--- src/Chirp.Razor/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Razor/Pages/AboutMe.cshtml.cs
@@ -5,8 +5,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Chirp.Razor.Pages;
     public class AboutMeModel : PageModel
     {
+        private readonly ICheepRepository _cheepRepository;
+        private readonly IAuthorRepository _authorRepository;
         public bool IsAuthenticated { get; private set; }
         public IEnumerable<Claim> FilteredClaims { get; private set; } = new List<Claim>();
+        public List<CheepDto> Cheeps { get; set; } = new List<CheepDto>();
+        public IEnumerable<string> Following { get; set; } = new List<string>();
+        public IEnumerable<string> Followers { get; set; } = new List<string>();
+
+        public AboutMeModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository)
+        {
+            _authorRepository = authorRepository;
+            _cheepRepository = cheepRepository;
+        }
+
         public void OnGet()
         {
             IsAuthenticated = User.Identity?.IsAuthenticated ?? false;
@@ -15,6 +27,9 @@ namespace Chirp.Razor.Pages;
             {
                 var desiredClaimTypes = new List<string> { "name", "emails" };
                 FilteredClaims = User.Claims.Where(c => desiredClaimTypes.Contains(c.Type));
+                Following = _authorRepository.GetAuthorFollowing(User.Identity!.Name!);
+                Followers = _authorRepository.GetAuthorFollowers(User.Identity!.Name!);
+                // Cheeps = (await _cheepRepository.GetCheepsFromAuthor(User.Identity!.Name!, pageIndex, 32)).ToList();
             }
         }
     }
