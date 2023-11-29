15d862733650cfb1ca363ec2ea5b95b9f8093d56
Rasmus <raln@itu.dk>
2023-11-29 13:37:06 +0100

    filter relevant claims for about me page

--- src/Chirp.Razor/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Razor/Pages/AboutMe.cshtml.cs
@@ -1,12 +1,20 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
-{
-    public class ClaimsModel : PageModel
+namespace Chirp.Razor.Pages;
+    public class AboutMeModel : PageModel
     {
+        public bool IsAuthenticated { get; private set; }
+        public IEnumerable<Claim> FilteredClaims { get; private set; } = new List<Claim>();
         public void OnGet()
         {
+            IsAuthenticated = User.Identity?.IsAuthenticated ?? false;
+
+            if (IsAuthenticated)
+            {
+                var desiredClaimTypes = new List<string> { "name", "emails" };
+                FilteredClaims = User.Claims.Where(c => desiredClaimTypes.Contains(c.Type));
+            }
         }
     }
-}
