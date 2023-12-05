3dc12eb83cbdf6526f573d4e0f751cda4c8f10b9
Rasmus <raln@itu.dk>
2023-12-05 15:34:43 +0100

    Fix follower count bug on userTimelines

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -11,6 +11,8 @@ public class UserTimelineModel : PageModel
     public List<CheepDto> Cheeps { get; set; }
     public IEnumerable<string> Following { get; set; }
     public IEnumerable<string> Followers { get; set; }
+    public int FollowingCount { get; private set; }
+    public int FollowersCount { get; private set; }
     
     [BindProperty]
     [StringLength(160)]
@@ -50,17 +52,24 @@ public class UserTimelineModel : PageModel
     }
     public async Task<IActionResult> OnGetAsync(string authorName, [FromQuery(Name = "page")] int pageIndex = 1)
     {
+
+        Following =  _authorRepository.GetAuthorFollowing(User.Identity!.Name!);
+        Followers = _authorRepository.GetAuthorFollowers(User.Identity!.Name!);
         if (IsCurrentAuthor(authorName))
         {
             Cheeps = (await _cheepRepository.GetPersonalCheeps(authorName, pageIndex, 32)).ToList();
+            FollowingCount = Following.Count();
+            FollowersCount = Followers.Count();
         }
         else
         {
             Cheeps = (await _cheepRepository.GetCheepsFromAuthor(authorName, pageIndex, 32)).ToList();
+            FollowingCount = _authorRepository.GetAuthorFollowing(authorName).Count();
+            FollowersCount = _authorRepository.GetAuthorFollowers(authorName).Count();
         }
 
-        Following =  _authorRepository.GetAuthorFollowing(User.Identity!.Name!);
-        Followers = _authorRepository.GetAuthorFollowers(User.Identity!.Name!);
+        
+
         return Page();
     }
 
