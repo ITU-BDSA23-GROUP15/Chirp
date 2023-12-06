abc7f73c7854e664705df2a38a1d729173ac4a51
Mads Nørklit Jensen <macj@itu.dk>
2023-12-06 15:08:07 +0100

    UI testing works on deployed website
    Need to make a new test suite that does
    not use a csproj file that requires our program.cs
    since we can't run localhost and the testsuite then.
    
    Co-authored-by: Jacob <jacp@itu.dk>
    Co-authored-by: Rasmus <raln@itu.dk>

--- test/Chirp.Testing/UiTests/UiTests.cs
+++ test/Chirp.Testing/UiTests/UiTests.cs
@@ -8,6 +8,8 @@ namespace Application.Testing;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
+
+// All tests in this class has been generated using codegen (https://playwright.dev/dotnet/docs/codegen) 
 public class UiTests : PageTest
 {
     [Test]
@@ -29,5 +31,48 @@ public class UiTests : PageTest
 
         // Expects the URL to contain intro.
         await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+    }
+ 
+    [Test]
+    public async Task MyTest()
+    {
+        await Page.GotoAsync("http://localhost:5273");
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "log in" }).ClickAsync();
+
+        await Page.GetByLabel("Username or email address").FillAsync("macj@itu.dk");
+
+        await Page.GetByLabel("Username or email address").PressAsync("Tab");
+
+        await Page.GetByLabel("Password").FillAsync("Ons24nov99");
+
+        await Page.GetByLabel("Password").PressAsync("Enter");
+
+        await Page.Locator("#Text").ClickAsync();
+
+        await Page.Locator("#Text").FillAsync("Hej virker denne test?");
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "about me" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+
+        await Page.Locator("li").Filter(new() { HasText = "Follow DanielSMillard — 12/06" }).GetByRole(AriaRole.Button).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+
+        await Page.Locator("li").Filter(new() { HasText = "Unfollow DanielSMillard Teeeeeeest — 12/06/2023 11:27:" }).GetByRole(AriaRole.Button).ClickAsync();
+
+        await Page.Locator("div").Filter(new() { HasText = "my timeline | public timeline" }).Nth(1).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Next page" }).ClickAsync();
+
     }
 }
