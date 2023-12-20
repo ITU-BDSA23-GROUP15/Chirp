c4848041abbcac0fcbb2ce232f41480c213f60e5
Daniel Millard <dmil@itu.dk>
2023-12-20 14:30:46 +0100

    Remove Deprecated GetCheepsFromFollowing Method

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -4,7 +4,6 @@ public interface ICheepRepository
 {
     Task<IEnumerable<CheepDto>> GetCheeps(int pageIndex, int pageRange);
     Task<IEnumerable<CheepDto>> GetCheepsFromAuthor(string authorName, int pageIndex, int pageRange);
-    Task<IEnumerable<CheepDto>> GetCheepsFromFollowing(string authorName, int pageIndex, int pageRange);
     Task<IEnumerable<CheepDto>> GetPersonalCheeps(string authorName, int pageIndex, int pageRange);
     Task CreateCheep(CreateCheepDto cheep);
     Task DeleteCheep(Guid cheepId);
--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -55,25 +55,7 @@ public class CheepRepository : ICheepRepository
             .Select(c => new CheepDto(c.Text, c.Author.Name, c.TimeStamp, c.CheepId))
             .ToListAsync();
     }
-
-    public async Task<IEnumerable<CheepDto>> GetCheepsFromFollowing(string authorName, int pageIndex, int pageRange)
-    {
-        var following = await _context.Authors
-            .Include(a => a.Following)
-            .Where(a => a.Name == authorName)
-            .SelectMany(a => a.Following)
-            .ToListAsync();
-
-        return await _context.Cheeps
-            .Include(c => c.Author)
-            .OrderByDescending(c => c.TimeStamp)
-            .Where(c => following.Contains(c.Author))
-            .Skip((pageIndex - 1) * pageRange)
-            .Take(pageRange)
-            .Select(c => new CheepDto(c.Text, c.Author.Name, c.TimeStamp, c.CheepId))
-            .ToListAsync();
-    }
-
+    
     public async Task CreateCheep(CreateCheepDto cheep)
     {
         var validationResult = await _validator.ValidateAsync(cheep);
